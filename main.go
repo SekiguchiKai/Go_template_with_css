caca71d270c1f2b537afb14203d84d22e2ec6fa5
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-02-23 14:41:53 +0900

    file created

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+func main() {
+	fmt.Println("The Server runs with http://localhost:8080/")
+	// ここで静的ファイルであるCSSを適用
+	// Handler : 第一引数で与えたパターンに対して、第二引数Handlerを登録
+	// StripPrefix : 第一引数で与えたURLのパスを取り除いて、第二引数Handlerを発動させる
+	// FileServer : 、HTTPリクエストに対して、第一引数のrootを起点とするファイルシステムのコンテンツを返すハンドラを返す
+	// ここでいうrootは、resources
+
+	// つまり、resourcesディレクトリ以下の静的ファイルを探して返す
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources/"))))
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+type Person struct {
+	Name string
+	From string
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	p := Person{
+		Name:"sekky",
+		From : "埼玉",
+	}
+
+	tmpl := template.Must(template.ParseFiles("./view/index.html"))
+	tmpl.Execute(w, p)
+}
